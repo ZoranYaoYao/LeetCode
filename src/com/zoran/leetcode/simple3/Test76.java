f95434d1182d57b7f79accfe58b01f12ad201292
zqs <zhengqingsong@didapinche.com>
2018-06-28 08:50:15 +0800

    * 两整数之和
     * https://leetcode-cn.com/submissions/detail/3585501/

---
+++ src/com/zoran/leetcode/simple3/Test76.java
@@ -0,0 +1,12 @@
+package com.zoran.leetcode.simple3;
+
+/**
+ * 两整数之和
+ * https://leetcode-cn.com/submissions/detail/3585501/
+ */
+public class Test76 {
+
+    public int getSum(int a, int b) {
+        return a + b;
+    }
+}
