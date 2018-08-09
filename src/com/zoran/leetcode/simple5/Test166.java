19f995f376a57f8c92416b03a35eeed392dcc8d6
zqs <zhengqingsong@didapinche.com>
2018-08-09 09:11:38 +0800

    * 转换成小写字母
     * https://leetcode-cn.com/submissions/detail/5309747/

---
+++ src/com/zoran/leetcode/simple5/Test166.java
@@ -0,0 +1,14 @@
+package com.zoran.leetcode.simple5;
+
+/**
+ * ×ª»»³ÉÐ¡Ð´×ÖÄ¸
+ * https://leetcode-cn.com/submissions/detail/5309747/
+ */
+public class Test166 {
+
+    public String toLowerCase(String str) {
+        if(str == null || str.length() == 0) return str;
+        
+        return str.toLowerCase();
+    }
+}
