fc6eb21329a44684db78ddc1e1070a412154f5f0
zqs <zhengqingsong@didapinche.com>
2018-09-21 09:46:17 +0800

    * 填充同一层的兄弟节点 II
     * https://leetcode-cn.com/submissions/detail/7384597/
     *
     * OT

---
+++ src/com/zoran/leetcode/medium2/Test65.java
@@ -0,0 +1,59 @@
+package com.zoran.leetcode.medium2;
+
+import java.util.LinkedList;
+
+import com.zoran.leetcode.util.NodeTest.TreeLinkNode;
+
+public class Test65 {
+
+    public void connect(TreeLinkNode root) {
+        //广度优先遍历 + LinkedList存储, 并对next赋值
+        if (root == null) return;
+        bfs(root);
+    }
+    
+    //breadth first search
+    public void bfs(TreeLinkNode root) {
+        LinkedList<TreeLinkNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i=0; i<size; i++) {
+                if (i+1<size) queue.get(i).next = queue.get(i+1);
+            }
+            
+            for(int i=0; i<size; i++) {
+                TreeLinkNode temp = queue.poll();
+                if (temp != null && temp.left != null) queue.add(temp.left);
+                if (temp != null && temp.right != null) queue.add(temp.right);
+            }
+        }
+    }
+}
+
+/**
+ * 将2个for循环 进行合并
+ */
+class LeetCode_Test65 {
+    public void connect(TreeLinkNode root) {
+        //广度优先遍历 + LinkedList存储, 并对next赋值
+        if (root == null) return;
+        bfs(root);
+    }
+    
+    //breadth first search
+    public void bfs(TreeLinkNode root) {
+        LinkedList<TreeLinkNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i=0; i<size; i++) {
+                if (i+1<size) queue.get(i).next = queue.get(i+1);
+                
+                TreeLinkNode temp = queue.poll();
+                if (temp != null && temp.left != null) queue.add(temp.left);
+                if (temp != null && temp.right != null) queue.add(temp.right);
+            }
+        }
+    }
+}
