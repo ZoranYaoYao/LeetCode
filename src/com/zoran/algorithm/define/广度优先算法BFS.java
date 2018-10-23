16f53aab62c6ab270f341e3499fa8a93c88bc995
zqs <zhengqingsong@didapinche.com>
2018-10-23 14:17:36 +0800

    分治算法定义 类似于动态规划

--- src/com/zoran/algorithm/define/广度优先算法BFS.java
+++ src/com/zoran/algorithm/define/广度优先算法BFS.java
@@ -4,7 +4,7 @@ package com.zoran.algorithm.define;
  * 广度优先算法BFS(Breadth First Search)
  * https://www.cnblogs.com/gczr/p/6476577.html
  */
-public class 广度优先算法 {
+public class 广度优先算法BFS {
 	
 //	 * 单词接龙
 //	 * https://leetcode-cn.com/submissions/detail/7524790/
