ff4173952eebbb58a3c353488944796d186555b4
Zoran <aa@qq.com>
2019-01-24 10:45:03 +0800

    * 函数的独占时间
     * https://leetcode-cn.com/submissions/detail/12027197/
     *
     * [Solution]
     * https://blog.csdn.net/gqk289/article/details/76062982
     *
     * 题意有歧义，看不懂 为啥+1

---
+++ src/com/zoran/leetcode/medium5/Test239.java
@@ -0,0 +1,13 @@
+package com.zoran.leetcode.medium5;
+
+/**
+ * 函数的独占时间
+ * https://leetcode-cn.com/submissions/detail/12027197/
+ *
+ * [Solution]
+ * https://blog.csdn.net/gqk289/article/details/76062982
+ *
+ * 题意有歧义，看不懂 为啥+1
+ */
+public class Test239 {
+}
