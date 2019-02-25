aa061d11e72ba3d0a442a18ca0d8a721a7def213
Zoran <aa@qq.com>
2019-02-25 11:41:06 +0800

    * 重新排列日志文件
     * https://leetcode-cn.com/submissions/detail/13314671/
     *
     * [Solution]
     * https://segmentfault.com/a/1190000016977854?utm_source=tag-newest
     *
     * Comparator外置比较器的记忆！ o1代表后一个元素！！

--- JavaApiTest/src/com/zoran/comparator/ComparatorTest.java
+++ JavaApiTest/src/com/zoran/comparator/ComparatorTest.java
@@ -5,6 +5,9 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Comparator
+ */
 public class ComparatorTest {
 
 
@@ -12,6 +15,15 @@ public class ComparatorTest {
         List<String> list = new ArrayList<>();
         list.add("a");list.add("b");list.add("c");
         Collections.sort(list, new Comparator<String>() {
+            /**??? 从源码上看，
+             * o1代表后面的一个数
+             * o2代表o1前面的一个数
+             *
+             * 如果return >0 则升序
+             * 如果return <0 则降序！
+             *
+             * 最后的结果的都是自然排序，就是说一定是升序！！
+             * */
             @Override
             public int compare(String o1, String o2) {
                 System.out.println("o1 = " + o1);
