9370d4747e8bbe40744da57e44c5b15ba79a4cca
Zoran <aa@qq.com>
2019-02-28 10:28:28 +0800

    * 重复 N 次的元素
     * https://leetcode-cn.com/submissions/detail/13544029/

---
+++ src/com/zoran/leetcode/simple6/Test331.java
@@ -0,0 +1,28 @@
+package com.zoran.leetcode.simple6;
+
+import java.util.Arrays;
+
+/**
+ * 重复 N 次的元素
+ * https://leetcode-cn.com/submissions/detail/13544029/
+ */
+public class Test331 {
+
+    public int repeatedNTimes(int[] A) {
+        //数组排序，对比次数
+        int N = A.length/2;
+        Arrays.sort(A);
+
+        int curNum = 1;
+        for (int i=0; i<A.length-1; i++) {
+            if (A[i] == A[i+1]) {
+                curNum++;
+            } else {
+                if (curNum == N) return A[i];
+                curNum = 1;
+            }
+        }
+
+        return A[A.length-1];
+    }
+}
